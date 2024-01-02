c06903ee3ab71351de5a7e9c62e6a5ebfed3b899
PKTripathi <tripathi.dbt@gmail.com>
2024-01-03 02:00:43 +0530

    done versel set

--- colorectal/settings.py
+++ colorectal/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'colorectal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.mysql',
@@ -113,7 +113,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
