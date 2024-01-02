0946f17dca7c239f9d5ca81adfe0cc5791b51e16
PKTripathi <tripathi.dbt@gmail.com>
2024-01-03 02:22:42 +0530

    done versel set

--- colorectal/urls.py
+++ colorectal/urls.py
@@ -20,6 +20,7 @@ from geno.views import genome_view
 from NaturalDrug.views import drug_view
 from fam.views import fam_view
 from metabol.views import metabolite_view
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from phase.views import home_view, about_view, contact_view, medical_view, factors_view, role_view, stages_view
 
 urlpatterns = [
@@ -49,3 +50,4 @@ urlpatterns = [
     path('',home_view)       
 ]
 
+urlpatterns += staticfiles_urlpatterns()
