c84e87b612bfe489a557c2f73137620deb33f2e8
PKTripathi <tripathi.dbt@gmail.com>
2024-01-03 01:39:16 +0530

    done versel set

--- colorectal/settings.py
+++ colorectal/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-8kn_74yee77wx&u8io)-2^a3$r3$^wx!bww7)c+i=cxy8%1(f^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'colorectal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.mysql',
@@ -113,7 +113,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -150,7 +150,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static') ,
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
