269b7d80ee427011c5bb0de5004bbc9a71750b43
PKTripathi <tripathi.dbt@gmail.com>
2024-01-04 15:53:39 +0530

    LOLZ

--- colorectal/settings.py
+++ colorectal/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-8kn_74yee77wx&u8io)-2^a3$r3$^wx!bww7)c+i=cxy8%1(f^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'https://crc-mmdb.onrender.com/']
 
 
 # Application definition
