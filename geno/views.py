1970866fe4cec27e7bc77640a4446ee6537485c2
PKTripathi <tripathi.dbt@gmail.com>
2024-01-04 15:57:48 +0530

    OK

--- geno/views.py
+++ geno/views.py
@@ -1,14 +1,14 @@
-from msilib.schema import ListView
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.db.models import Q
 from .models import Genome_crc
-    
-    #genome view
-class genome_view(ListView):
-        template_name= "genome.html"
-    
-        def get_queryset(self):
-            query = self.request.GET.get("q", default="1")
-            object_list = Genome_crc.objects.filter(Q(GeneID=query))
-            return object_list
+
+# Genome view
+class GenomeView(ListView):
+    template_name = "genome.html"
+    model = Genome_crc  # Specify the model for the ListView
+
+    def get_queryset(self):
+        query = self.request.GET.get("q", default="1")
+        object_list = Genome_crc.objects.filter(Q(GeneID=query))
+        return object_list
